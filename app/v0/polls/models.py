61dcba56b2618fb06b059e7edaa9c21d13ae2dff
Nathan Williams <nlw2sx@virginia.edu>
2016-10-07 15:04:29 -0400

    Make runnable

--- app/v0/polls/models.py
+++ app/v0/polls/models.py
@@ -3,15 +3,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-#I don't want to delete this one in case it breaks the site
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
-    
-#this is the start of our actual tables
 class Contest(models.Model):
     title = models.CharField(max_length=128, primary_key=True)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -20,7 +11,7 @@ class Contest(models.Model):
         return self.title
 
 class Question(models.Model):
-    contest = models.ForeignKey(Contest, on_delete=models.CASCADE, primary_key=True)
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
     question_number = models.IntegerField(primary_key=True)
     question_text = models.CharField(max_length=2048)
     submission = models.CharField(max_length=128) #title of document submission
@@ -34,9 +25,9 @@ class Team(models.Model):
         return self.name
 
 class Submissions(models.Model):
-    contest = models.ForeignKey(Contest, on_delete=models.CASCADE, primary_key=True)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, primary_key=True)
-    question_number = models.ForeignKey(Question, on_delete=models.CASCADE, primary_key=True)
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    question_number = models.ForeignKey(Question, on_delete=models.CASCADE)
     submission = models.CharField(max_length=128) #title of the document submission
     def __str__(self):
         return "CONTEST: " + self.contest + "\n" + "TEAM: " + self.team + "\n" + "QUESTION: " + self.question_number + "\n" + "SUBMISSION: " + self.submission
