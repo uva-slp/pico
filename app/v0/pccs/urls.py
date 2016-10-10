156272f8331a1036f15520c5ebcda41e5fb62b2b
Nathan Williams <nlw2sx@virginia.edu>
2016-10-10 16:06:55 -0400

    Add github hook

--- app/v0/pccs/urls.py
+++ app/v0/pccs/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     # url(r'^', include('main.urls', namespace='main')),
     url(r'^admin/', admin.site.urls),
+    url(r'^github-hook$', views.github_hook, name='github-hook'),
     url(r'^$', views.index, name='index'),
 ]
--- app/v0/pccs/views.py
+++ app/v0/pccs/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.views import generic
 
+import subprocess
+import os
+
 def index(request):
     return HttpResponse('Hello world!')
+
+def github_hook(request):
+	if os.path.isfile('/home/slp/pccs/html/github-hook.php'):
+		subprocess.call('php /home/slp/pccs/html/github-hook.php')
+		return HttpResponse('Success!')
+	raise Http404
