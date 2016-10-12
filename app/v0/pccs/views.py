c06b6f41b553e17372e1485e5f4dc6647b8c661f
Nathan Williams <nlw2sx@virginia.edu>
2016-10-11 23:14:35 -0400

    Git web hook test 3

--- app/v0/pccs/views.py
+++ app/v0/pccs/views.py
@@ -3,13 +3,21 @@ from django.shortcuts import render
 from django.views import generic
 
 import os
-import subprocess
+from subprocess import Popen, PIPE
 
 def index(request):
-    return HttpResponse('Hello world!')
+    return HttpResponse('Hello world! How are you?')
 
 def github_hook(request):
 	if os.path.isfile('/home/slp/pccs/html/github-hook.php'):
-		output = subprocess.check_output('php /home/slp/pccs/html/github-hook.php', shell=True)
-		return HttpResponse('Success!<br><br>%s'%(output))
+		p = Popen(['php','/home/slp/pccs/html/github-hook.php'], shell=True, stdout=PIPE, stderr=PIPE)
+		output, errors = p.communicate()
+		if not p.returncode and not errors:
+			return HttpResponse('Success!')
+		return HttpResponseServerError('Failed to pull from git repo.<br><br>%s<br><br>%s'%(output, errors))
 	return HttpResponseNotFound('Unable to find github hook script.')
+
+
+
+		# output = subprocess.check_output('php /home/slp/pccs/html/github-hook.php', shell=True)
+		# return HttpResponse('Success!<br><br>%s'%(output))
