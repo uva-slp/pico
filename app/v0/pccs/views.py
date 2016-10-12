e62eab0b42eaffb0d748f29f246608278ed05def
Nathan Williams <nlw2sx@virginia.edu>
2016-10-11 23:32:31 -0400

    Git web hook test 3

--- app/v0/pccs/views.py
+++ app/v0/pccs/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import generic
 
 import os
+import subprocess
 from subprocess import Popen, PIPE
 
 def index(request):
@@ -10,14 +11,14 @@ def index(request):
 
 def github_hook(request):
 	if os.path.isfile('/home/slp/pccs/html/github-hook.php'):
-		p = Popen(['php','/home/slp/pccs/html/github-hook.php'], stdout=PIPE, stderr=PIPE, env=os.environ)
-		output, errors = p.communicate()
-		if not p.returncode and not errors:
-			return HttpResponse('Success!')
-		return HttpResponseServerError('Failed to pull from git repo.<br><br>%s<br><br>%s'%(output, errors))
+		
+		output = subprocess.check_output('php /home/slp/pccs/html/github-hook.php', shell=True)
+		return HttpResponse('Success!<br><br>%s'%(output))
+
+		# p = Popen(['php','/home/slp/pccs/html/github-hook.php'], stdout=PIPE, stderr=PIPE, env=os.environ)
+		# output, errors = p.communicate()
+		# if not p.returncode and not errors:
+		# 	return HttpResponse('Success!')
+		# return HttpResponseServerError('Failed to pull from git repo.<br><br>%s<br><br>%s'%(output, errors))
+	
 	return HttpResponseNotFound('Unable to find github hook script.')
-
-
-
-		# output = subprocess.check_output('php /home/slp/pccs/html/github-hook.php', shell=True)
-		# return HttpResponse('Success!<br><br>%s'%(output))
