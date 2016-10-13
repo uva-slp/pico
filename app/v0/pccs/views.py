0372c3619a433e4c6dd8241f92401c7f1412f403
Nathan Williams <nlw2sx@virginia.edu>
2016-10-13 10:16:02 -0400

    Git web hook final test

--- app/v0/pccs/views.py
+++ app/v0/pccs/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.views import generic
 
 def index(request):
-    return HttpResponse('Hello world! How are you?')
+    return HttpResponse('Hello world!')
