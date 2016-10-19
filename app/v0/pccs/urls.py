0cf6dca6759b1a8a7362b83ac08c8930f5abe8d4
Nathan Williams <nlw2sx@virginia.edu>
2016-10-19 14:54:53 -0400

    Changes urls

--- app/v0/pccs/urls.py
+++ app/v0/pccs/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
-	url(r'^users/', include('users.urls', namespace='users')),
-	url(r'^contests/', include('contests.urls', namespace='contests')),
+	url(r'^u/', include('users.urls', namespace='users')),
+	url(r'^c/', include('contests.urls', namespace='contests')),
 	url(r'^$', views.index, name='index'),
 ]
