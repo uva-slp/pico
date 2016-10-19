4ccd1086575bed1af067a3644144244d3f1c7f2e
Nathan Williams <nlw2sx@virginia.edu>
2016-10-18 23:15:45 -0400

    Diff with hardcoded test input

--- app/v0/contests/views.py
+++ app/v0/contests/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 
+from difflib import HtmlDiff # temporary
+
+from lib import diff as _diff
+
 def home(request):
 	return HttpResponse('home')
+
+def diff(request):
+	fromlines = ['foo', 'bar', 'flarp']
+	tolines = ['food', 'jip', 'bar', 'zoo', 'jaslkdfj;laskdjflasdlkflasdgflkjghkljcvkljadkjlfhkajsldfhlkawheuifyiasdhflkjashdjklfhkajsdhflkjashiudfyoiauwhelkfjhaslkjdfhklasjhdfkljashdkflj']
+
+	html, numChanges = _diff.HtmlFormatter(fromlines, tolines).asTable()
+
+	return render(
+		request,
+		'contests/diff.html',
+		{'diff_table': html, 'numChanges': numChanges})
