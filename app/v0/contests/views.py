495cd2e67fd68442a6088e229720ff477453dbce
Nathan Williams <nlw2sx@virginia.edu>
2016-10-21 14:02:52 -0400

    Force Travis run

--- app/v0/contests/views.py
+++ app/v0/contests/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 
 def home(request):
-	return HttpResponse('home page')
+	j = "foo"
+	foo = "Make Travis run"
+	please="please"
+	please == please
+	return HttpResponse('home page %s'%(please+foo+j))
