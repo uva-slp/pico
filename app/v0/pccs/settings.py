5be5c9384c70f0d462bb167eaffaf10abffb2771
Nathan Williams <nlw2sx@virginia.edu>
2016-10-21 16:07:31 -0400

    Migrations for testing

--- app/v0/pccs/settings.py
+++ app/v0/pccs/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-import sys
 import secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -89,6 +88,7 @@ DATABASES = {
 }
 
 # Travis CI
+import sys
 if 'test' in sys.argv:
 	DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 	DATABASES['default']['NAME'] = os.path.join(BASE_DIR, 'db.sqlite3')
