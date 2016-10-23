7842f8c593caeedaf89eb6676ae2b7d5825205eb
Nathan Williams <nlw2sx@virginia.edu>
2016-10-23 17:04:57 -0400

    Format users/admin.py

--- app/v0/users/admin.py
+++ app/v0/users/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.models import User as AuthUser, Group
 
 from .models import User, Team
-
 
 admin.site.unregister(AuthUser)
 admin.site.unregister(Group)
