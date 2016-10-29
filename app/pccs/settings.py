b0509f4ca022d989f42fdf1af3b1650c30fe20e0
Nathan Williams <nlw2sx@virginia.edu>
2016-10-29 17:01:47 -0400

    DateTimePicker currently incompatible with our Django version

--- app/pccs/settings.py
+++ app/pccs/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-	'bootstrap3_datetime',
+	# 'bootstrap3_datetime',
 ]
 
 MIDDLEWARE = [
