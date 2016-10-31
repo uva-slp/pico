13b3ac14c47f2fc4e3eb091d65e182d0040e6078
Nathan Williams <nlw2sx@virginia.edu>
2016-10-31 12:54:56 -0400

    Fix everything

--- app/contests/forms.py
+++ app/contests/forms.py
@@ -70,8 +70,8 @@ class CreateContestTemplate(ModelForm):
 class CreateContestForm(forms.Form):
     title = forms.CharField(label='Title', max_length=32)
     languages = forms.CharField(label='Languages', widget=forms.CheckboxSelectMultiple)
-    length = forms.CharField(label='Length', choices=CONTEST_LENGTH)
-    autojudge = forms.CharField(label='Autojudge', choices=AUTOJUDGE)
+    length = forms.ChoiceField(label='Length', choices=CONTEST_LENGTH)
+    autojudge = forms.ChoiceField(label='Autojudge', choices=AUTOJUDGE)
     ##file stuff - need group input on how this should be laid out
     ##problems = forms.FileField()
     ##answers = forms.FileField()
