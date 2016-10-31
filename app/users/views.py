b5e067e8aea3e940b9c85f855798c67f3ae6fb66
vivianbuan <wl2nw@virginia.edu>
2016-10-31 17:08:58 -0400

    Clear unused import

--- app/users/views.py
+++ app/users/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout, update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import password_reset
 from django.urls import reverse
 
 from common.decorators import anonymous_required
