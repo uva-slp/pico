e36d152323540cdb01090741fb81f8d477e5ef5b
Vivian Liu <wl2nw@virginia.edu>
2016-10-31 19:35:04 -0400

    Revert "Login"

--- app/users/views.py
+++ app/users/views.py
@@ -6,7 +6,6 @@ from django.urls import reverse
 
 from common.decorators import anonymous_required
 from .forms import UserForm, LoginForm
-from pccs.emailserver import SERVER_EMAIL
 
 @anonymous_required
 def register(request):
