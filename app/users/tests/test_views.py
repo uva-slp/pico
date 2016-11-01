c799f4e60345fd7377591c45a56c7d4c9edd4b70
vivianbuan <wl2nw@virginia.edu>
2016-11-01 02:43:09 -0400

    Add tests for password reset view

--- app/users/tests/test_views.py
+++ app/users/tests/test_views.py
@@ -47,3 +47,44 @@ class LogoutTest(TestCase):
 		resp = self.client.get(url)
 
 		self.assertRedirects(resp, reverse('index'), status_code=302, target_status_code=200)
+
+class PasswordChangeTest(TestCase):
+
+	fixtures = ['users.json']
+
+	def test_get(self):
+		self.client.login(username='testuser', password='password')
+		url = reverse('users:password_change')
+		resp = self.client.get(url)
+
+		self.assertEqual(resp.status_code, 200)
+
+class PasswordResetTest(TestCase):
+
+	fixtures = ['users.json']
+
+	def test_get_form(self):
+		url = reverse('users:password_reset')
+		resp = self.client.get(url)
+
+		self.assertEqual(resp.status_code, 200)
+
+	def test_get_done(self):
+		url = reverse('users:password_reset_done')
+		resp = self.client.get(url)
+
+		self.assertEqual(resp.status_code, 200)
+
+	def test_get_complete(self):
+		url = reverse('users:password_done')
+		resp = self.client.get(url)
+
+		self.assertEqual(resp.status_code, 200)
+
+	def test_post(self):
+		data = {
+			'email': 'test@test.com'
+		}
+		resp = self.client.post(reverse('users:password_reset'), data=data)
+
+		self.assertRedirects(resp, reverse('users:password_reset_done'), status_code=302, target_status_code=200)
