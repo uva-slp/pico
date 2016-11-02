4e123aa1bb574930432a84c50ae3a626ded3b657
Nathan Williams <nlw2sx@virginia.edu>
2016-11-02 12:37:44 -0400

    Anonymous required on index page

--- app/pccs/views.py
+++ app/pccs/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.views import generic
 
+from common.decorators import anonymous_required
 from users.forms import LoginForm, UserForm
 
+@anonymous_required
 def index(request):
 	return render(
 		request,
