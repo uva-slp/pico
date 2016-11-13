238ab38135e4ebe23add32008fea330d3f6d31fd
DerekMcMahon <dmm7aj@virginia.edu>
2016-11-13 14:05:25 -0500

    Improve code submission error handling

--- app/contests/lib/execution.py
+++ app/contests/lib/execution.py
@@ -13,7 +13,7 @@ import shutil
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-
+#Returns a return code and the output of the program or an error message
 def execute_code(file):
     file_name, file_extension = os.path.splitext(file.name)
     #Check if it's a Java file:
@@ -24,9 +24,10 @@ def execute_code(file):
     if file_extension in cpp_extensions:
         return run_cpp(file)
     #If it didn't have a proper extensions, can't compile and run it:
-    return "Must have a valid C++ or Java file extension"
+    return (1, "FILENAME ERROR: Must have a valid C++ or Java file extension")
 
 
+#Returns the a return code and the output of the program or an error message as a tuple.
 def run_java(file):
     temp_dirpath = tempfile.mkdtemp()
     file_name = file.name
@@ -36,17 +37,20 @@ def run_java(file):
     compilation_result = Popen("javac " + os.path.join(temp_dirpath, file_name), shell=True, stdout=PIPE, stderr=PIPE)
     compiled_file = os.path.splitext(file_name)[0]
     output, error = compilation_result.communicate()
-    retval = ''
+    retval = (1, '')
     if compilation_result.returncode !=0:
-        retval = "THE ERROR IS: " + str(error.decode("utf-8"))
+        retval = (1, ("COMPILATION ERROR:\n" + str(error.decode("utf-8"))))
     else:
         program_output = Popen("java -cp " + temp_dirpath + " " + compiled_file, shell=True, stdout=PIPE, stderr=PIPE)
         output, error = program_output.communicate()
-        retval = output
+        if program_output.returncode != 0:
+            retval = (1, ("EXECUTION ERROR:\n" + str(error.decode("utf-8"))))
+        else:
+            retval = (0, output.decode())
     shutil.rmtree(temp_dirpath)
     return retval
 
-
+#Returns the a return code and the output of the program or an error message as a tuple.
 def run_cpp(file):
     temp_dirpath = tempfile.mkdtemp()
     file_name = file.name
@@ -56,11 +60,14 @@ def run_cpp(file):
     compilation_result = Popen("/usr/bin/g++ " + os.path.join(temp_dirpath, file_name) + " -o " + os.path.join(temp_dirpath, 'a.out'), shell=True, stdout=PIPE, stderr=PIPE)
     output, error = compilation_result.communicate()
     retval = ''
-    if compilation_result.returncode !=0:
-        retval = "THE ERROR IS: " + str(error.decode("utf-8"))
+    if compilation_result.returncode != 0:
+        retval = (1, ("COMPILATION ERROR:\n" + str(error.decode("utf-8"))))
     else:
         program_output = Popen(os.path.join(temp_dirpath, './a.out'), shell=True, stdout=PIPE, stderr=PIPE)
         output, error = program_output.communicate()
-        retval = output
+        if program_output.returncode != 0:
+            retval = (1, ("EXECUTION ERROR:\n" + str(error.decode("utf-8"))))
+        else:
+            retval = (0, output.decode())
     shutil.rmtree(temp_dirpath)
     return retval
