e53e1099e5a114eb2fb2f8dd6b309a8ca7dad17f
easiestripes <adp4fm@virginia.edu>
2016-11-14 00:46:17 -0500

    Fixed tests
    Refactored test functions and used SimpleUploadedFile to test pdf uploads

--- app/contests/tests.py
+++ app/contests/tests.py
@@ -1,35 +1,34 @@
 from django.test import TestCase
-from .forms import CreateContestTemplate
 from .forms import CreateContestForm
-from .models import ContestTemplate
+from .models import Contest
 from django.urls import reverse
 from django.shortcuts import render
-from .models import Contest
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 
-class ContestTemplateTest(TestCase):
+class ContestTest(TestCase):
 
 	fixtures = ['forms.json']
 
 	# models test
-	def contest_template(
+	def contest(
 			self, title="only a test", languages="java, python",
 			length="02:00", penalty="20", autojudge="0", review="",
 			desc="problems.pdf", admins="", participants=""):
-		return ContestTemplate.objects.create(
+		return Contest.objects.create(
 			title=title, languages=languages,
 			contest_length=length, time_penalty=penalty,
 			autojudge_enabled=autojudge, autojudge_review=review,
 			problem_description=desc, contest_admins=admins,
 			contest_participants=participants)
 
-	def test_contest_template_creation(self):
-		ct = self.contest_template()
-		self.assertTrue(isinstance(ct, ContestTemplate))
+	def test_contest_creation(self):
+		ct = self.contest()
+		self.assertTrue(isinstance(ct, Contest))
 		self.assertEqual(ct.__str__(), ct.title)
 
-	def test_contest_template_db_entry(self):
-		ct = ContestTemplate.objects.get(pk=1)
+	def test_contest_db_entry(self):
+		ct = Contest.objects.get(pk=1)
 		self.assertEqual(ct.title, 'Contest 1')
 		ct.title = "Updated Contest 1"
 		ct.save()
@@ -38,37 +37,43 @@ class ContestTemplateTest(TestCase):
 	# forms test
 	def test_valid_form(self):
 		data = {
-			"title": "Contest 1", "languages": "java, python",
+			"title": "Contest 1", "creator": 1, "languages": "java, python",
 			"contest_length": "02:00", "time_penalty": "20",
 			"autojudge_enabled": "0", "autojudge_review": "",
+			"problem_description": "problems.pdf",
 			"contest_admins": "", "contest_participants": ""
 		}
-		form = CreateContestTemplate(data=data)
+		files = {
+			"problem_description": SimpleUploadedFile("problems.pdf", b"test content")
+		}
+		form = CreateContestForm(data=data, files=files)
+		print form.errors
 		self.assertTrue(form.is_valid())
 
 	def test_empty_form_fields(self):
 		data = {
 			"title": "", "languages": "", "contest_length": "",
 			"time_penalty": "", "autojudge_enabled": "0", "autojudge_review": "",
-			"contest_admins": "", "contest_participants": ""
+			"problem_description": "", "contest_admins": "",
+			"contest_participants": ""
 		}
-		form = CreateContestTemplate(data=data)
+		form = CreateContestForm(data=data)
 		self.assertFalse(form.is_valid())
 
 	# views test
-	def test_create_template_view(self):
-		url = reverse("contests:createTemplate")
+	def test_create_view(self):
+		url = reverse("contests:create")
 		resp = self.client.get(url)
 
 		self.assertEqual(resp.status_code, 200)
-##
-		
+
+'''
 class ContestTest(TestCase):
 
 	fixtures = ['contests.json']
 
 	# model test
-	def contest_template(
+	def contest(
 		self, title="only a test", languages="Python",
 		length="02:00", autojudge="0",
 		desc="problems.pdf", solution="solutions.txt", admins="",
@@ -119,31 +124,31 @@ class ContestTest(TestCase):
 		resp = self.client.get(url)
 
 		self.assertEqual(resp.status_code, 200)
-##
-
+'''
+'''
 class SubmissionsViewsTest(TestCase):
 	fixtures = ['submission.json']
 	
-	def test_choose_question_page(self):
-		response = self.client.get(reverse('contests:choose_question'))
+	def test_choose_problem_page(self):
+		response = self.client.get(reverse('contests:choose_problem'))
 		self.assertEqual(response.status_code, 200)
 
-	def test_choose_question_page_template(self):
-		response = self.client.get(reverse('contests:choose_question'))
+	def test_choose_problem_page_template(self):
+		response = self.client.get(reverse('contests:choose_problem'))
 		self.assertTemplateUsed(response, 'contests/choose_problem.html')
 
-	def test_choose_question_page_title(self):
-		response = self.client.get(reverse('contests:choose_question'))
-		self.assertContains(response, 'Questions:')
+	def test_choose_problem_page_title(self):
+		response = self.client.get(reverse('contests:choose_problem'))
+		self.assertContains(response, 'Problems:')
 
 	def test_upload_code_page(self):
-		response = self.client.get(reverse('contests:upload_code', kwargs = {'question_id': '1'}))
+		response = self.client.get(reverse('contests:upload_code', kwargs = {'problem_id': '1'}))
 		self.assertEqual(response.status_code, 200)
 
 	def test_upload_code_page_title(self):
-		response = self.client.get(reverse('contests:upload_code', kwargs = {'question_id': '1'}))
-		self.assertContains(response, "Submit for QuestionExample")
-
+		response = self.client.get(reverse('contests:upload_code', kwargs = {'problem_id': '1'}))
+		self.assertContains(response, "Submit for ProblemExample")
+'''
 class ContestCreationTest(TestCase):
 
 	def testNumberofContests(self):
