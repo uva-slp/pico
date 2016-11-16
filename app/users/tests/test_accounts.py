9a8f877d509895b6d4b8d9c4e985f827e8f2a022
jason <jgd3hb@virginia.edu>
2016-11-16 03:14:41 -0500

    fixed test cases i think

--- app/users/tests/test_accounts.py
+++ app/users/tests/test_accounts.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import reverse
 from django.test import TestCase
 from .models import User
@@ -9,7 +8,7 @@ class InvalidUserInfoTest(TestCase):
         u = User(username='buddy1', password='password', email='buddyatgmail.com')
         u.save()
         if(teams.filter(username='buddy1')):
-=======
+
 from django.test import TestCase
 from users.models import User
 
@@ -19,38 +18,25 @@ class InvalidUserInfoTest(TestCase):
         u = User(username='buddy1', password='password', email='buddyatgmail.com')
         u.save()
         if(users.filter(username='buddy1')):
->>>>>>> fc5676706f54ec8eef029e02844dfdf9fe946b46
             self.assertTrue('@' not in u.email)
 
     def invalid_first_name(self):
         u = User(username='buddy2', password='password', first_name='123', email='buddy@gmail.com')
         u.save()
-<<<<<<< HEAD
-        if(teams.filter(username='buddy2')):
-=======
         if(users.filter(username='buddy2')):
->>>>>>> fc5676706f54ec8eef029e02844dfdf9fe946b46
             self.assertFalse(any(c.isalpha() for c in u.first_name))
 
     def invalid_last_name(self):
         u = User(username='buddy3', password='password', last_name='123', email='buddy@gmail.com')
         u.save()
-<<<<<<< HEAD
-        if(teams.filter(username='buddy3')):
-=======
         if(users.filter(username='buddy3')):
->>>>>>> fc5676706f54ec8eef029e02844dfdf9fe946b46
             self.assertFalse(any(c.isalpha() for c in u.last_name))
 
 class ValidUserInfoTest(TestCase):
     def valid_email(self):
         u = User(username='buddy4', password='password', email='buddy@gmail.com')
         u.save()
-<<<<<<< HEAD
-        if(teams.filter(username='buddy4')):
-=======
         if(users.filter(username='buddy4')):
->>>>>>> fc5676706f54ec8eef029e02844dfdf9fe946b46
             self.assertTrue('@' in u.email)
             self.assertTrue('.' in u.email)
             self.assertTrue(any(c.isalpha() for c in u.email))
@@ -58,21 +44,13 @@ class ValidUserInfoTest(TestCase):
     def valid_first_name(self):
         u = User(username='buddy5', password='password', first_name='buddy', email='buddy@gmail.com')
         u.save()
-<<<<<<< HEAD
-        if(teams.filter(username='buddy5')):
-            self.assertTrue(c in 'abcdefghijklmnopqrstuvwxyz' for c in u.first_name)
-=======
+
         if(users.filter(username='buddy5')):
             self.assertTrue(all(c.isalpha() for c in u.first_name))
->>>>>>> fc5676706f54ec8eef029e02844dfdf9fe946b46
 
     def valid_last_name(self):
         u = User(username='buddy6', password='password', last_name='friend', email='buddy@gmail.com')
         u.save()
-<<<<<<< HEAD
-        if(teams.filter(username='buddy6')):
-            self.assertTrue(c in 'abcdefghijklmnopqrstuvwxyz' for c in u.last_name)
-=======
         if(users.filter(username='buddy6')):
             self.assertTrue(all(c.isalpha() for c in u.last_name))
 
@@ -105,4 +83,3 @@ class NoInjectionsPleaseTest(TestCase):
         u.save()
         if(users.filter(username='buddy')):
             self.assertEqual(u.username, 'buddy')
->>>>>>> fc5676706f54ec8eef029e02844dfdf9fe946b46
