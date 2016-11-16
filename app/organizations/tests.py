94e11434adfdcc5f4103db4af1e842547c222516
jason <jgd3hb@virginia.edu>
2016-11-16 03:09:41 -0500

    incremental test casing

--- app/organizations/tests.py
+++ app/organizations/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ValidOrganizationNameTest(TestCase):
+    #form should prevent invalid characters from being entered and that len() > 0
+    def invalid_org_name(self):
+        o1 = Organization(name='')
+        o2 = Organization(name='123org...()')
+        o1.save()
+        o2.save()
+        self.assertFalse(len(o1.name) > 0)
+        self.assertFalse(all(c.isalnum() for c in o2.name))
