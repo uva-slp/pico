feb65882b9b71274e3ad2f7236fd7a86923bc26f
vivianbuan <wl2nw@virginia.edu>
2016-11-27 15:59:18 -0500

    Read from file

--- app/contests/templatetags/contest_extras.py
+++ app/contests/templatetags/contest_extras.py
@@ -5,3 +5,14 @@ register = template.Library()
 @register.filter
 def index(sequence, position):
 	return sequence[position-1]
+
+
+@register.filter
+def print_file_content(f):
+	if not f.name:
+		return ''
+	else:
+		try:
+			return f.read()
+		except IOError:
+			return ''
