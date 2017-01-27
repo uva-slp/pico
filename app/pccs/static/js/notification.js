40f572e74310b87f3e5ad82008d201a72c36b1e1
vivianbuan <wl2nw@virginia.edu>
2017-01-27 16:26:17 -0500

    Fetch every 5s, show content in modal on success

--- app/pccs/static/js/notification.js
+++ app/pccs/static/js/notification.js
@@ -1,8 +1,23 @@
-$.ajax({
-    url: "/c/api/get_notification/",
-    data: {
-    },
-    success: function (data) {
-        alert(data);
-    }
-});
+function fetchNotification() {
+
+
+    $.ajax({
+        url: "/c/api/get_notification/",
+        success: function (data) {
+            renderNotification(data);
+            console.log(data);
+        },
+        complete: function () {
+            setTimeout(fetchNotification, 5000);
+        }
+    });
+}
+
+// fetch from serve
+setTimeout(fetchNotification, 5000);
+
+// Assume we've already have a string
+function renderNotification(data) {
+    $('#modalBody').html(JSON.stringify(data));
+    $('#myModal').modal('show');
+}
