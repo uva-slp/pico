2fafb63e659d52ef4943eef08900f6ec488903df
the_deng <jdeng4@gmail.com>
2017-02-13 07:33:25 -0500

    test cases

--- app/teams/tests/tests_misc.py
+++ app/teams/tests/tests_misc.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from teams.models import Team
+from teams.models import Team, Invite, JoinRequest
+from users.models import User
 
 class TeamTestCases(TestCase):
 
@@ -39,3 +40,58 @@ class TeamTestCases(TestCase):
         team = Team(name="team")
 
         self.assertEqual(str(team), "team")
+
+
+class InviteTestCases(TestCase):
+
+    #jason 
+    def testInviteCount(self):
+        u1 = User(username='buddy1', password='jgd3hb111', email='buddy1@gmail.com')
+        u2 = User(username='buddy2', password='jgd3hb222', first_name='123', email='buddy2@gmail.com')
+        t = Team(name='team')
+        u1.save()
+        u2.save()
+        t.save()
+        i1 = Invite(team=t, user=u1)
+        i2 = Invite(team=t, user=u2)
+        i1.save()
+        i2.save()
+        invite_count = Invite.objects.all().count()
+        self.assertEqual(invite_count, 2)
+
+    #jason
+    def testInviteIdentity(self):
+        #we should change this, you shouldnt be able to invite yourself to a team you created
+        u1 = User(username='buddy1', password='jgd3hb111', email='buddy1@gmail.com')
+        t = Team(name='team')
+        u1.save()
+        t.save()
+        t.members.add(u1)
+        i = Invite(team=t, user=u1)
+        i.save()
+        invite_count = Invite.objects.all().count()
+        self.assertEqual(invite_count, 1)
+
+    #jason
+    def testJoinRequest(self):
+        u = User(username='buddy1', password='jgd3hb111', email='buddy1@gmail.com')
+        t = Team(name='team')
+        u.save()
+        t.save()
+        j = JoinRequest(team=t, user=u)
+        j.save()
+        join_request_count = JoinRequest.objects.all().count()
+        self.assertEqual(join_request_count, 1)
+
+    #jason
+    def testJoinRequestIdentity(self):
+        #we should change this, you shouldnt be able to send a join request to your own team
+        u = User(username='buddy1', password='jgd3hb111', email='buddy1@gmail.com')
+        t = Team(name='team')
+        u.save()
+        t.save()
+        t.members.add(u)
+        j = JoinRequest(team=t, user=u)
+        j.save()
+        join_request_count = JoinRequest.objects.all().count()
+        self.assertEqual(join_request_count, 1)
