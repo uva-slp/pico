68deff820a535e6d51313ff5bf4a581fded7dfe5
vivianbuan <wl2nw@virginia.edu>
2017-02-13 23:24:28 -0500

    Five more test cases

--- app/contests/tests/tests_views.py
+++ app/contests/tests/tests_views.py
@@ -5,13 +5,67 @@ from contests.forms import ReturnJudgeResultForm, CreateContestForm
 from contests.models import Team, Participant, Contest, Problem, Submission, ContestTemplate
 
 
-# class ContestViewTest(TestCase):
-#
-#     fixtures = ['judge_interface.json']
-#
-# 	# Vivianu
-# 	def test_view_contest_judge(self):
+class ContestViewTest(TestCase):
 
+    fixtures = ['judge_interface.json']
+
+    # Vivian
+    def test_view_contest_judge(self):
+        self.client.login(username='judge', password='password')
+        user = auth.get_user(self.client)
+        assert user.is_authenticated()
+
+        url = reverse("contests:contest", kwargs={'contest_id': 7})
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+    # Vivian
+    def test_view_contest_admin(self):
+        self.client.login(username='myadmin', password='password')
+        user = auth.get_user(self.client)
+        assert user.is_authenticated()
+
+        url = reverse("contests:contest", kwargs={'contest_id': 7})
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+    # # Vivian
+    # TODO: this need to be refactored after the contest participant/admin field is changed
+    # def test_view_contest_participant(self):
+    #     self.client.login(username='participant1', password='password')
+    #     user = auth.get_user(self.client)
+    #     assert user.is_authenticated()
+    #
+    #     url = reverse("contests:contest", kwargs={'contest_id': 7})
+    #     resp = self.client.get(url)
+    #     self.assertEqual(resp.status_code, 200)
+
+    # Vivian
+    def test_view_contest_superuser(self):
+        self.client.login(username='admin', password='password')
+        user = auth.get_user(self.client)
+        assert user.is_authenticated()
+
+        url = reverse("contests:contest", kwargs={'contest_id': 7})
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+
+    # Vivian
+    def test_view_contest_nonparticipant(self):
+        self.client.login(username='nonparticipant', password='password')
+        user = auth.get_user(self.client)
+        assert user.is_authenticated()
+
+        url = reverse("contests:contest", kwargs={'contest_id': 7})
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+
+
+    # Vivian
+    def test_view_contest_nonloggedin(self):
+        url = reverse("contests:contest", kwargs={'contest_id': 7})
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
 
 
 class JudgeInterfaceViewTest(TestCase):
@@ -21,6 +75,9 @@ class JudgeInterfaceViewTest(TestCase):
     # Vivian
     def test_view_all_judge(self):
         self.client.login(username='judge', password='password')
+        user = auth.get_user(self.client)
+        assert user.is_authenticated()
+
         url = reverse("contests:contest_judge_submissions",
                       kwargs={'contest_id': 7})
         resp = self.client.get(url)
