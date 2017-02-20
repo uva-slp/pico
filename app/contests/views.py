df7e61cf1e1e325125eca22a58723c3be0e8a890
the_deng <jdeng4@gmail.com>
2017-02-20 06:21:08 -0500

    stat page added

--- app/contests/views.py
+++ app/contests/views.py
@@ -480,5 +480,21 @@ def close_notification(request):
     return HttpResponse('OK')
 
 def stats(request):
-    contest_participation = Participant.objects.all()
-    return render(request, 'contests/stats.html', {'contest_participation' : contest_participation})
+    if request.user.is_authenticated():
+        participation = Participant.objects.filter(team__members__username=request.user.username).order_by('contest__date_created')
+        contest_count = Participant.objects.filter(team__members__username=request.user.username).count()
+        teams = Team.objects.filter(members__username=request.user.username).order_by('name')
+        teams_count = teams.count()
+        teammates_count = 0
+        for t in teams:
+            members = t.members.all()
+            for m in members:
+                if m.username != request.user.username:
+                    teammates_count += 1
+        return render(request, 'contests/stats.html', { 'participation' : participation, 
+                                                        'contest_count' : contest_count,
+                                                        'teams' : teams,
+                                                        'teams_count' : teams_count,
+                                                        'teammates_count' : teammates_count})
+    else:
+        return render(request, 'contests/stats.html')
