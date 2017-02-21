05cb81cc23b6d0719e8114cd2b963561b9f317ee
nathan-williams <nlw2sx@virginia.edu>
2017-02-20 20:43:50 -0500

    Remove Django tag from Javascript file

--- app/teams/static/js/team-stickytabs.js
+++ app/teams/static/js/team-stickytabs.js
@@ -1,3 +1,5 @@
 $(document).on('click', '#team-tabs a', function(event) {
+    var baseURL = window.location.href;
+    baseURL = baseURL.substr(0, baseURL.indexOf('/teams/')+7);
     history.replaceState('data', '', '{% url "teams:index" %}' + this.href.split('#team-')[1] + '/');
 });
