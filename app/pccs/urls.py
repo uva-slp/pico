00739b375c2112e3be41c043bdf5ceb542bee5e4
DerekMcMahon <dmm7aj@virginia.edu>
2017-02-26 15:26:16 -0500

    Wrap up Javascript code coverage

--- app/pccs/urls.py
+++ app/pccs/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
 	url(r'^contests/', include('contests.urls', namespace='contests')),
 	url(r'^$', views.index, name='index'),
 	url(r'^home$', views.home, name='home'),
-        url(r'^qunit$', views.qunit, name='qunit'),
 ]
 
