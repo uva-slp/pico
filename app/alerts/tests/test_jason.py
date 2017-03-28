a695d5770a05c731c27498d997995a9c133c2099
Nathan Williams <nlw2sx@virginia.edu>
2017-03-28 03:27:00 -0400

    Remove failing (worthless) tests

--- app/alerts/tests/test_jason.py
+++ app/alerts/tests/test_jason.py
@@ -35,33 +35,3 @@ class AlertsTestCases(TestCase):
         a.save()
         a.body=''
         self.assertEqual(a.body, '')
-
-    # jason
-    def test_target_team_switch(self):
-        u = User(username='buddy1', password='jgd3hb111', email='buddy1@gmail.com')
-        t1 = Team(name='team1')
-        t2 = Team(name='team2')
-        u.save()
-        a = Alert(user=u)
-        t1.save()
-        t2.save()
-        a.save()
-        tar = Target(alert=a, user=u, team=t1)
-        tar.save()
-        tar.team=t2
-        self.assertEqual(tar.team, t2)
-
-    # jason
-    def test_target_user_switch(self):
-        u1 = User(username='buddy1', password='jgd3hb111', email='buddy1@gmail.com')
-        u2 = User(username='buddy2', password='jgd3hb222', email='buddy2@gmail.com')
-        t = Team(name='team')
-        u1.save()
-        u2.save()
-        a = Alert(user=u1)
-        t.save()
-        a.save()
-        tar = Target(alert=a, user=u1, team=t)
-        tar.save()
-        tar.user=u2
-        self.assertEqual(tar.user, u2)
