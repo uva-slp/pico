055d783b892b9535a919b63cd85f39280bdb6949
Nathan Williams <nlw2sx@virginia.edu>
2017-03-27 23:49:31 -0400

    Updated coverage docs

--- test/js/tests/test-users.js
+++ test/js/tests/test-users.js
@@ -23,7 +23,35 @@ QUnit.test( "submitUserEditForm test", function( assert ) {
 // Theme picker
 
 QUnit.test( "loadThemes test", function( assert ) {
-    loadThemes();
-    $.getJSONBackup = $.getJSON;
+    assert.expect(1);
+    syncThemeInput('');
+    loadThemesFailed();
+    loadThemes(assert.async());
+    assert.ok( 1 == "1", "Passed!" );
+});
+QUnit.test( "submitChangeThemeForm test", function( assert ) {
+    replaceAjax(function(options) {
+        options.success({theme:''},null,{status:200});
+        options.success({error:''},null,{status:201});
+        options.error(null);
+    });
+    submitChangeThemeForm({preventDefault:function(){}});
+    restoreAjax();
+    assert.ok( 1 == "1", "Passed!" );
+});
+
+// Storage
+
+QUnit.test( "renderDiskUsageChart test", function( assert ) {
+    hasRenderedDiskUsageChart = null;
+    tooltipFormatter.call({y:0,key:'',series:{name:''}}, null);
+    var chart = $('<div/>')
+        .attr('id', 'storage')
+        .addClass('active')
+        .append($('<div/>')
+            .attr('id', 'disk-usage-chart'));
+    $('body').append(chart);
+    renderDiskUsageChart();
+    chart.remove();
     assert.ok( 1 == "1", "Passed!" );
 });
