af759ca5676f5e3c3c534da384944ac4bd62f4c6
Nathan Williams <nlw2sx@virginia.edu>
2017-03-30 09:39:35 -0400

    Exclude manage.py from coverage report

--- test/python/coverage-report.sh
+++ test/python/coverage-report.sh
@@ -2,5 +2,5 @@
 cd ../../app
 coverage3 run --source="." manage.py test
 coverage3 report --include="$PWD/*" --omit="$PWD/manage.py" -m
-coverage3 html -d ../docs/coverage
+coverage3 html --include="$PWD/*" --omit="$PWD/manage.py" -d ../docs/coverage
 mv .coverage ../test/python/.coverage
