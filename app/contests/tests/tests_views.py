7e08b443cc010841c6096c5e727785c334a6e94d
vivianbuan <wl2nw@virginia.edu>
2017-04-10 16:34:54 -0400

    Add new submission to cover missing line

--- app/contests/tests/tests_views.py
+++ app/contests/tests/tests_views.py
@@ -876,6 +876,15 @@ class NotificationViewTest(TestCase):
         user = auth.get_user(self.client)
         assert user.is_authenticated()
 
+        test_contest = Contest.objects.get(id=7)
+        test_team = Team.objects.get(id=1)
+        participant = Participant(contest=test_contest, team=test_team)
+        participant.save()
+        problems = test_contest.problem_set.all()
+        problem = list(problems)[0]
+        test_submission = Submission(run_id=1, team=test_team, problem=problem, timestamp=datetime.now(timezone.utc), state="NEW")
+        test_submission.save()
+
         data = {"contestId": 7}
         resp = self.client.post(reverse("contests:refresh_submission"), data=data)
         self.assertEqual(resp.status_code, 200)
