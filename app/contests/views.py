d9a41cdb6c1a12ba113b68669e2c32e1d848b7ac
Nathan Williams <nlw2sx@virginia.edu>
2017-04-12 10:17:03 -0400

    Remove line exclusion from coverage config

--- app/contests/views.py
+++ app/contests/views.py
@@ -530,30 +530,6 @@ def displayJudge(request, contest_id, run_id):
                         html, numChanges = _diff.HtmlFormatter(fromlines, tolines, False).asTable()
                         return render(request, 'contests/judge.html', {'diff_table': html, 'numChanges': numChanges, 'contest_data': contest_data, 'is_judge': True, 'submission': current_submission, 'form': form})
 
-
-def stats(request):
-    if request.user.is_authenticated():
-        participation = Participant.objects.filter(team__members__username=request.user.username).order_by('contest__date_created')
-        contest_count = Participant.objects.filter(team__members__username=request.user.username).count()
-        teams = Team.objects.filter(members__username=request.user.username).order_by('name')
-        teams_count = teams.count()
-        teammates_count = 0
-        for t in teams:
-            members = t.members.all()
-            for m in members:
-                if m.username != request.user.username:
-                    teammates_count += 1
-
-        data = {
-            'participation' : participation, 'contest_count' : contest_count,
-            'teams' : teams, 'teams_count' : teams_count, 'teammates_count' : teammates_count
-        }
-
-        return render(request, 'contests/stats.html', data)
-    else:
-        return render(request, 'contests/stats.html')
-
-
 def scoreboard(request, contest_id):
     #contest_id = request.POST.get('contestId', "0")
 
