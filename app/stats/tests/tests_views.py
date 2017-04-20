3113c1a8f04d8aaf2fd908708acfd3f06eeef76d
vivianbuan <wl2nw@virginia.edu>
2017-04-19 23:58:22 -0400

    Cover line of submission=yes

--- app/stats/tests/tests_views.py
+++ app/stats/tests/tests_views.py
@@ -1,10 +1,13 @@
 from django.contrib import auth
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
+from django.utils import timezone
+from contests.models import Team, Contest, Problem, Participant,Submission
+from datetime import datetime
 
 
 class DisplayContestTest(TestCase):
-    fixtures = ['users.json', 'teams.json', 'contests.json']
+    fixtures = ['users.json', 'teams.json', 'contests.json', 'problems.json']
 
     def setUp(self):
         self.factory = RequestFactory()
@@ -20,6 +23,14 @@ class DisplayContestTest(TestCase):
         self.client.login(username='participant1', password='password')
         user = auth.get_user(self.client)
         assert user.is_authenticated()
+
+        test_contest = Contest.objects.get(id=22)
+        test_team = Team.objects.get(id=3)
+        participant = Participant(contest=test_contest, team=test_team)
+        participant.save()
+        problem = Problem.objects.get(id=1)
+        test_submission = Submission(run_id=1, team=test_team, problem=problem, timestamp=datetime.now(timezone.utc), state="YES", result="YES")
+        test_submission.save()
 
         url = reverse("stats:index")
         resp = self.client.get(url)
