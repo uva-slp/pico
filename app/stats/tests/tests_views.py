b3ef736ce6452f37835be05c7c8ccb2ca116471c
vivianbuan <wl2nw@virginia.edu>
2017-04-19 23:46:55 -0400

    Add simple stat test

--- app/stats/tests/tests_views.py
+++ app/stats/tests/tests_views.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.contrib import auth
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+
+
+class DisplayContestTest(TestCase):
+    fixtures = ['users.json', 'teams.json', 'contests.json']
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    # Vivian
+    def test_view_stat_participant(self):
+        self.client.login(username='participant1', password='password')
+        user = auth.get_user(self.client)
+        assert user.is_authenticated()
+
+        url = reverse("stats:index")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
 
 
