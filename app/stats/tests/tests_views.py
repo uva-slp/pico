3516be54bbba601e55b15a645213c69341a1115e
vivianbuan <wl2nw@virginia.edu>
2017-04-19 23:53:34 -0400

    Change check of user login in stat view and add test

--- app/stats/tests/tests_views.py
+++ app/stats/tests/tests_views.py
@@ -10,6 +10,12 @@ class DisplayContestTest(TestCase):
         self.factory = RequestFactory()
 
     # Vivian
+    def test_view_stat_notloggedin(self):
+        url = reverse("stats:index")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 302)
+
+    # Vivian
     def test_view_stat_participant(self):
         self.client.login(username='participant1', password='password')
         user = auth.get_user(self.client)
