3b3d712aba1e3e316a7d937dee1c16a72d50946d
vivianbuan <wl2nw@virginia.edu>
2017-04-24 00:28:06 -0400

    Change fixture to cover submission status=yes line

--- test/js/tests/test-contests.js
+++ test/js/tests/test-contests.js
@@ -81,8 +81,8 @@ QUnit.module("notification setup", {
         var fixture = $("#qunit-fixture");
         fixture.append('<div id="getNotificationUrl" style="display: none;">http://localhost:8000/contests/api/get_notification/</div>');
         data_list = [];
-        false_data = ('test contest', 1, 1, 'False', 1, 1);
-        yes_data = ('test contest', 1, 2, 'Yes', 2, 2);
+        false_data = ['test contest', 1, 1, 'False', 1, 1];
+        yes_data = ['test contest', 1, 2, 'Yes', 2, 2];
         data_list.push(yes_data);
         data_list.push(false_data);
     }
