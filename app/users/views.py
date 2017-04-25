10377c7763c184c2ad51dfd9244dbfc834d8bd30
Nathan Williams <nlw2sx@virginia.edu>
2017-04-25 18:47:58 -0400

    Fix password issue

--- app/users/views.py
+++ app/users/views.py
@@ -36,46 +36,43 @@ def view(request, user_id=None):
 
 @anonymous_required
 def register(request):
-	if request.method == 'POST':
-		user_form = UserForm(data=request.POST)
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
 
-		if user_form.is_valid():
-			user = user_form.save(commit=False)
-			user.set_password(user.password)
-			user.save()
+        if user_form.is_valid():
+            user = user_form.save()
+            auth_login(request, user)
 
-			auth_login(request, user)
+            return redirect(reverse('home'))
+    else:
+        user_form = UserForm()
 
-			return redirect(reverse('home'))
-	else:
-		user_form = UserForm()
-
-	return render(
-		request,
-		'users/register.html',
-		{'user_form': user_form})
+    return render(
+        request,
+        'users/register.html',
+        {'user_form': user_form})
 
 @anonymous_required
 def login(request):
-	if request.method == 'POST':
-		login_form = LoginForm(data=request.POST)
+    if request.method == 'POST':
+        login_form = LoginForm(data=request.POST)
 
-		if login_form.is_valid():
-			auth_login(request, login_form.user_cache)
-			return redirect(reverse('home'))
+        if login_form.is_valid():
+            auth_login(request, login_form.user_cache)
+            return redirect(reverse('home'))
 
-	else:
-		login_form = LoginForm()
+    else:
+        login_form = LoginForm()
 
-	return render(
-		request,
-		'users/login.html',
-		{'login_form': login_form})
+    return render(
+        request,
+        'users/login.html',
+        {'login_form': login_form})
 
 def logout(request):
-	if request.user.is_authenticated():
-		auth_logout(request)
-	return redirect(reverse('index'))
+    if request.user.is_authenticated():
+        auth_logout(request)
+    return redirect(reverse('index'))
 
 @login_required
 def edit(request):
@@ -163,21 +160,21 @@ def settings(request):
 
 @login_required
 def password_change(request):
-	if request.method == 'POST':
-		password_change_form = PasswordChangeForm(request.user, data=request.POST)
+    if request.method == 'POST':
+        password_change_form = PasswordChangeForm(request.user, data=request.POST)
 
-		if password_change_form.is_valid():
-			password_change_form.save()
-			update_session_auth_hash(request, password_change_form.user)
-			return redirect(reverse('home'))
+        if password_change_form.is_valid():
+            password_change_form.save()
+            update_session_auth_hash(request, password_change_form.user)
+            return redirect(reverse('home'))
 
-	else:
-		password_change_form = PasswordChangeForm(request.user)
+    else:
+        password_change_form = PasswordChangeForm(request.user)
 
-	return render(
-		request,
-		'users/password_change.html',
-		{'password_change_form': password_change_form})
+    return render(
+        request,
+        'users/password_change.html',
+        {'password_change_form': password_change_form})
 
 class UserAutocomplete(autocomplete.Select2QuerySetView):
 
