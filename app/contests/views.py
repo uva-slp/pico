bb135007628b4dd9413a864c1f8c910ac1dc7235
easiestripes <adp4fm@virginia.edu>
2017-05-06 18:25:35 -0400

    Attempted fix for reuploading files in edit contest

--- app/contests/views.py
+++ app/contests/views.py
@@ -194,6 +194,7 @@ def editContest(request, contest_id):
         return HttpResponseForbidden()
 
     form = CreateContestForm(None, None, instance=contest)
+    form.base_fields['problem_description'].required = False
 
     admin_search_form = AdminSearchForm(initial={'contest_admins': contest.contest_admins.all()})
     participant_search_form = ParticipantSearchForm(initial={'contest_participants': contest.contest_participants.all()})
