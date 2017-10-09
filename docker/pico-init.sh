b2bfead01abcc3a29f4870375c172a90ce822856
Aaron Bloomfield <aaron@virginia.edu>
2017-10-09 14:14:48 -0400

    Improved the Dockerfile, which reduced the image size

--- docker/pico-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -d "/init-db/" ]; then
-    sleep 5
-    cat /init-db/000-db-init.sql | mysql
-    cat /init-db/schema.sql | mysql pico
-    /bin/rm /init-db/*
-    rmdir /init-db
-fi
